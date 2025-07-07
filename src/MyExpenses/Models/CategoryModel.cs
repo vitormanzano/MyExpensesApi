12d787fa2e019752834683847b3acdbfeb4bec55
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-07-07 17:03:15 -0300

    feat: Adding constructor in others models
    Using the same pattern to set the properties in my constructor

--- src/MyExpenses/Models/CategoryModel.cs
+++ src/MyExpenses/Models/CategoryModel.cs
@@ -8,5 +8,34 @@
         public UserModel User { get; set; }
 
         protected CategoryModel() { }
+
+        public CategoryModel(string name, Guid userId)
+        {
+            Id = Guid.NewGuid();
+            SetName(name);
+            SetUserId(userId);
+        }
+
+        public void SetName(string name)
+        {
+            ValidateName(name);
+            Name = name;
+        }
+
+        private void ValidateName(string name)
+        {
+            switch (name.Length)
+            {
+                case < 4:
+                    throw new ArgumentOutOfRangeException("Name must have at least 4 characters");
+                case > 255:
+                    throw new ArgumentOutOfRangeException("Name must have less than 255 characters");
+            }
+        }
+
+        public void SetUserId(Guid userId)
+        {
+            UserId = userId;
+        }
     }
 }
--- src/MyExpenses/Models/ExpenseModel.cs
+++ src/MyExpenses/Models/ExpenseModel.cs
@@ -11,5 +11,41 @@
         public CategoryModel Category { get; set; }
 
         protected ExpenseModel() { }
+
+        public ExpenseModel(decimal value, DateOnly date, Guid userId, Guid categoryId)
+        {
+            Id = Guid.NewGuid();
+            SetValue(value);
+            SetDate(date);
+            SetUserId(userId);
+            SetCategoryId(categoryId);
+        }
+
+        public void SetValue(decimal value)
+        {
+            ValidateValue(value);
+            Value = value;
+        }
+
+        private void ValidateValue(decimal value)
+        {
+            if (value <= 0)
+                throw new ArgumentException("Value must be greater than zero");
+        }
+
+        public void SetDate(DateOnly date)
+        {
+            Date = date;
+        }
+        
+        public void SetUserId(Guid userId)
+        {
+            UserId = userId;
+        }
+
+        public void SetCategoryId(Guid categoryId)
+        {
+            CategoryId = categoryId;
+        }
     }
 }
