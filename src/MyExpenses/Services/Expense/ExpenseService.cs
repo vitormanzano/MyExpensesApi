d51c4895ce87fa12c2d7023f9ef5edd052518f67
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-07-27 22:19:32 -0300

    feat: Methods to Find expenses service
    Adding find methods in the services to comunicate with the repository

--- src/MyExpenses/Services/Expense/ExpenseService.cs
+++ src/MyExpenses/Services/Expense/ExpenseService.cs
@@ -2,27 +2,21 @@
 using MyExpenses.Mappers;
 using MyExpenses.Models;
 using MyExpenses.Repository.Expense;
+using MyExpenses.Services.Exceptions;
 
 namespace MyExpenses.Services.Expense
 {
-    public class ExpenseService : IExpenseService
+    public class ExpenseService(IExpenseRepository expenseRepository) : IExpenseService
     {
-        private readonly IExpenseRepository _expenseRepository;
-
-        public ExpenseService(IExpenseRepository expenseRepository)
-        {
-            _expenseRepository = expenseRepository;
-        }
-
-        public async Task<ResponseExpenseDto> CreateExpense(CreateExpenseDto createExpenseDto, Guid UserId)
+        public async Task<ResponseExpenseDto> CreateExpense(CreateExpenseDto createExpenseDto, Guid userId)
         {
             var expenseModel = new ExpenseModel(
                 createExpenseDto.Value,
                 createExpenseDto.Date,
-                UserId,
+                userId,
                 createExpenseDto.CategoryId);
 
-            var expenseDb = await _expenseRepository.CreateExpense(expenseModel);
+            var expenseDb = await expenseRepository.CreateExpense(expenseModel);
 
             var expenseResponse = expenseDb.MapExpenseToResponseExpenseDto();
 
@@ -31,10 +25,51 @@ namespace MyExpenses.Services.Expense
 
         public async Task<List<ResponseExpenseDto>> FindAllExpenses(Guid userId)
         {
-            var expenses = await _expenseRepository.FindAllExpenses(userId);
+            var expenses = await expenseRepository.FindAllExpenses(userId);
 
             var expensesResponse = expenses.Select(x => x.MapExpenseToResponseExpenseDto()).ToList();
+            return expensesResponse;
+        }
 
+        public async Task<ResponseExpenseDto> FindExpenseById(Guid id)
+        {
+            var expense = await expenseRepository.FindExpenseById(id) ?? throw new NotFoundException("Expense not found!");
+            
+            var expenseResponse = expense.MapExpenseToResponseExpenseDto();
+            return expenseResponse;
+        }
+
+        public async Task<List<ResponseExpenseDto>> FindExpensesByValue(Guid userId, decimal value)
+        {
+            if (value <= 0)
+                throw new ArgumentException("Expense value must be greater than zero!");
+
+            var expenses = await expenseRepository.FindExpensesByValue(userId, value);
+            
+            if (expenses.Count == 0)
+                throw new NotFoundException("None expense was found!");
+            
+            var expensesResponse = expenses.Select(x => x.MapExpenseToResponseExpenseDto()).ToList();
+            return expensesResponse;
+        }
+
+        public async Task<List<ResponseExpenseDto>> FindExpenseByMonth(Guid userId, int month, int year)
+        {
+            if (month < 1 || month > 12)
+                throw new ArgumentException("Month must be between 1 and 12!");
+            
+            if (year < 1900 || year > DateTime.Now.Year + 10)
+                throw new ArgumentException("Invalid year!");
+            
+            var startDate = new DateOnly(year, month, 1);
+            var endDate = startDate.AddMonths(1);
+            
+            var expenses = await expenseRepository.FindExpensesByDate(userId, startDate, endDate);
+            
+            if (expenses.Count == 0)
+                throw new NotFoundException("None expense was found!");
+            
+            var expensesResponse = expenses.Select(x => x.MapExpenseToResponseExpenseDto()).ToList();
             return expensesResponse;
         }
     }
--- src/MyExpenses/Services/Expense/IExpenseService.cs
+++ src/MyExpenses/Services/Expense/IExpenseService.cs
@@ -6,5 +6,8 @@ namespace MyExpenses.Services.Expense
     {
         Task<ResponseExpenseDto> CreateExpense(CreateExpenseDto createExpenseDto, Guid UserId);
         Task<List<ResponseExpenseDto>> FindAllExpenses(Guid userId);
+        Task<ResponseExpenseDto> FindExpenseById(Guid id);
+        Task<List<ResponseExpenseDto>> FindExpensesByValue(Guid userId, decimal value);
+        Task<List<ResponseExpenseDto>> FindExpenseByMonth(Guid userId, int month, int year);
     }
 }
