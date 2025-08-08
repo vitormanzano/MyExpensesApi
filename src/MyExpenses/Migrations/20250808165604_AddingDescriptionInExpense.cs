cf45c659a498e9d700d51d1bab3bce79c98290b4
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-08-08 15:04:23 -0300

    feat: Adding description in expenses
    Adding the possibility to write a description about the expense, but not
    required

---
+++ src/MyExpenses/Migrations/20250808165604_AddingDescriptionInExpense.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyExpenses.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingDescriptionInExpense : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Expenses",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Expenses");
+        }
+    }
+}
