5a88921ac31d67b04cbc9266ac49e3f6e4b84676
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2026-06-30 13:25:21 -0300

    refactor: Using standard record constructor

--- src/MyExpenses/Dtos/Category/ResponseCategoryDto.cs
+++ src/MyExpenses/Dtos/Category/ResponseCategoryDto.cs
@@ -1,13 +1,4 @@
 ﻿namespace MyExpenses.Dtos.Category
 {
-    public record ResponseCategoryDto
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public ResponseCategoryDto(Guid id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
-    }
+    public record ResponseCategoryDto(Guid Id, string Name);
 }
